1fd7b0299794bb264b29ce2398c59326b7f80915
raphnov <raphael.novena@yahoo.com.br>
2018-01-23 23:00:04 -0200

    templating

--- apolo/polls/views.py
+++ apolo/polls/views.py
@@ -2,21 +2,31 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
+# from django.template import loader
 
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
+    # try:
+    #    question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #    raise Http404("Question does not exist!")
+    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+    # return HttpResponse("You are looking at question %s" % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
