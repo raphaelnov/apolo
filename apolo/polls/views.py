c949c3bb77f86935d102f8974cf393808025c5b7
raphnov <raphael.novena@yahoo.com.br>
2018-01-25 19:39:51 -0200

    testing

--- apolo/polls/views.py
+++ apolo/polls/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponseRedirect #, HttpResponse, Http404
 # from django.template import loader
 
 from django.urls import reverse
-
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from .models import Question, Choice
@@ -16,13 +16,19 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions (not including those set to be published in the future)."""
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """Excludes any question that aren't published yet"""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
